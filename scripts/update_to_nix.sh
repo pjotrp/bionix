2a213e0237cf0d45c4b4068216ad60d0f5911bba
Pjotr <pjotr.public05@thebird.nl>
2008-10-07 16:58:29 +0200

    Another script

---
+++ scripts/update_to_nix.sh
@@ -0,0 +1,4 @@
+#! /bin/bash
+
+cp -vau applications/* ../nixpkgs-testing-pjotr/pkgs/applications/science/biology/
+cp -vau libraries/* ../nixpkgs-testing-pjotr/pkgs/development/libraries/science/biology/
